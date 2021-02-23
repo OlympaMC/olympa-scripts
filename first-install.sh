f7ca8f6e9c319970656a5611f58adca1fe4ba0b0
Tristiisch <tristiisch74@hotmail.com>
2021-02-23 04:02:30 +0100

    up

--- first-install.sh
+++ first-install.sh
@@ -1,1 +1,19 @@
+##   ____  _
+##  / __ \| |
+## | |  | | |_   _ _ __ ___  _ __   __ _
+## | |  | | | | | | '_ ` _ \| '_ \ / _` |
+## | |__| | | |_| | | | | | | |_) | (_| |
+##  \____/|_|\__, |_| |_| |_| .__/ \__,_|
+##            __/ |         | |
+##           |___/          |_|
+##
+## Install all requirement and add default value to OS
+## Need to be run as root
+
 apt-get install portsentry fail2ban htop nano nmap mariadb-server default-jre jq
+
+echo resources/profile >> /etc/profile
+ln -s resources/firewall /bin/
+ln -s resources/mc /bin/
+chmod 755 firewall
+chmod 755 mc
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ln -s firewall /bin/
-ln -s mc /bin/
-chmod 755 firewall
-chmod 755 mc
