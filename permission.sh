0f8e15e1f0e99dfff8d1bbe3abd4259f8c777384
Tristiisch <tristiisch74@hotmail.com>
2021-05-16 01:49:32 +0200

    up

---
+++ permission.sh
@@ -0,0 +1,7 @@
+/bin/setfacl -m u:minecraft:--- /usr/bin/mysql
+/bin/setfacl -m u:bullobily:--- /usr/bin/mysql
+/bin/setfacl -m u:ayfri:--- /usr/bin/mysql
+
+/bin/setfacl -m u:minecraft:--- /usr/bin/redis-cli
+/bin/setfacl -m u:bullobily:--- /usr/bin/redis-cli
+/bin/setfacl -m u:ayfri:--- /usr/bin/redis-cli
