c4478600652e612f4b2ff77c9a51a8f14adc8195
WeedoBot <5581957-WeedoBot@users.noreply.gitlab.com>
2021-06-09 06:43:29 +0200

    up multi user mc

--- permission.sh
+++ permission.sh
@@ -1,7 +1,9 @@
 /bin/setfacl -m u:minecraft:--- /usr/bin/mysql
 /bin/setfacl -m u:bullobily:--- /usr/bin/mysql
 /bin/setfacl -m u:ayfri:--- /usr/bin/mysql
+/bin/setfacl -m u:loockeeer:--- /usr/bin/mysql
 
 /bin/setfacl -m u:minecraft:--- /usr/bin/redis-cli
 /bin/setfacl -m u:bullobily:--- /usr/bin/redis-cli
 /bin/setfacl -m u:ayfri:--- /usr/bin/redis-cli
+/bin/setfacl -m u:loockeeer:--- /usr/bin/redis-cli
